52146e1a30068747e209448da04f1dca36468cea
xixi-tech-hogwards <xixi@testerhome.com>
2020-03-30 16:57:40 +0800

    add others

--- pytest_demos/pytest_fixture_demo/test_params/test_params.py
+++ pytest_demos/pytest_fixture_demo/test_params/test_params.py
@@ -4,9 +4,14 @@ import pytest
 import yaml
 
 
-@pytest.mark.parametrize("a,b", yaml.safe_load(open("datas.yml",encoding='utf-8')))
-def test_foo(a,b):
+@pytest.mark.parametrize("a,b", yaml.safe_load(open("datas.yml", encoding='utf-8')))
+def test_foo(a, b):
     print(f"a + b = {a + b}")
+
 
 def test_yaml():
     print(yaml.safe_load(open("datas.yml")))
+
+
+def test_a():
+    print("testa")
